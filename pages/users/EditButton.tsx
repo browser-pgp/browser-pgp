dac66fa4ffef1ba9622ce2fd6e6d733a61c11ed9
shynome <shynome@gmail.com>
2020-03-28 01:33:38 +0800

    编辑按钮就够了

--- pages/users/EditButton.tsx
+++ pages/users/EditButton.tsx
@@ -1,21 +1,12 @@
-import { Menu, MenuItem, IconButton } from '@material-ui/core'
-import { Fragment, useRef, useState } from 'react'
+import { IconButton } from '@material-ui/core'
 import EditIcon from '@material-ui/icons/Edit'
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import { useImportUser } from './ImportUser'
 
 export const EditButton = (props: { id: string }) => {
-  const ref = useRef()
-  const [open, setOpen] = useState(false)
-
+  const u = useImportUser()
   return (
-    <Fragment>
-      <IconButton ref={ref} onClick={() => setOpen(true)}>
-        <MoreHorizIcon />
-      </IconButton>
-      <Menu anchorEl={ref.current} open={open} onClose={() => setOpen(false)}>
-        <MenuItem onClick={() => setOpen(false)}>编辑</MenuItem>
-        <MenuItem onClick={() => setOpen(false)}>删除</MenuItem>
-      </Menu>
-    </Fragment>
+    <IconButton onClick={() => u.open(props.id)}>
+      <EditIcon />
+    </IconButton>
   )
 }
