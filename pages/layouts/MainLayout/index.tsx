122d42daaf7ed4e2d95c7e94741c310b688cfea9
shynome <shynome@gmail.com>
2020-04-01 18:21:30 +0800

    add inContainer option

--- pages/layouts/MainLayout/index.tsx
+++ pages/layouts/MainLayout/index.tsx
@@ -1,17 +1,34 @@
 import { Fragment } from 'react'
 import Head from 'next/head'
+import { Container } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
 
 import Dashboard from './Dashboard'
 
+const useStyles = makeStyles(theme => ({
+  croot: {
+    height: '100%',
+    padding: theme.spacing(3, 0),
+  },
+}))
+
 export const MainLayout: React.StatelessComponent<{
   title: string
+  inContainer?: boolean
 }> = props => {
+  const classes = useStyles()
   return (
     <Fragment>
       <Head>
         <title>{props.title} PGP in Browser</title>
       </Head>
-      <Dashboard>{props.children}</Dashboard>
+      <Dashboard>
+        {props.inContainer ? (
+          <Container className={classes.croot}>{props.children}</Container>
+        ) : (
+          props.children
+        )}
+      </Dashboard>
     </Fragment>
   )
 }
